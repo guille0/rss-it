2c8fd4e732aea9d0859183aa1f6bc1b5c5e25826
guille0 <guillermovicente@protonmail.com>
2019-05-25 10:41:09 +0200

    lengths

--- gui/rss.py
+++ gui/rss.py
@@ -1,21 +1,6 @@
 from __future__ import unicode_literals
 from feedgen.feed import FeedGenerator
 # sudo pip3 install feedgen
-import youtube_dl
-
-
-def video_to_mp3(video_id):
-    # Gets video id and returns mp3 (youtube-dl)
-    ydl_opts = {
-        'quiet': True,
-        'format': 'bestaudio/best',
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(video_id, download=False)
-        # print(info)
-    return info['formats'][0]['url'], info['formats'][0]['filesize']
-
-# video_to_mp3('-R6l_UERq_Q')
 
 
 class Rss:
@@ -32,15 +17,14 @@ class Rss:
         if logo:
             self.fg.logo(logo)
 
-    def add_video(self, video_id, title, pubdate, description=''):
+    def add_video(self, link, title, pubdate, description=''):
         fe = self.fg.add_entry()
-        link, length = video_to_mp3(video_id)
         fe.id(link)
         fe.title(title)
         # Get the first paragraph of the description?
         fe.description(description)
         fe.pubDate(pubdate)
-        fe.enclosure(url=link, length=length type='audio/mpeg')
+        fe.enclosure(link, '9999999999', 'audio/mpeg')
 
     def export(self):
         return self.fg.rss_str(pretty=True)
--- rss_it/settings.py
+++ rss_it/settings.py
@@ -20,13 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY='t0z(052o2t_xl5j*^1ni@q#wlp0e+p+hgmrb8x9j3qj%i4mqn2'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(os.environ.get('DEBUG'))
 DEBUG = True
 
-ALLOWED_HOSTS = ['rss-it.herokuapp.com']
+ALLOWED_HOSTS = [
+    'rss-it.herokuapp.com',
+    '127.0.0.1']
 
 
 # Application definition
