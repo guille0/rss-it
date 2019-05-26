e6b5fc5d32bfeb319e1a3b7f9a51a887b2ff53c8
guille0 <guillermovicente@protonmail.com>
2019-05-26 10:14:52 +0200

    debug=false

--- rss_it/settings.py
+++ rss_it/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get('DEBUG_VALUE', False)
+DEBUG = False
+# DEBUG = os.environ.get('DEBUG_VALUE', False)
 
 ALLOWED_HOSTS = [
     'rss-it.herokuapp.com',
